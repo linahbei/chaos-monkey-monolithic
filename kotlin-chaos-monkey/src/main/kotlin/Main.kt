5634fd85ce4de9ae5f7e0d38e8226adebf4ed720
Ahbei <ahbei.lin@gmail.com>
2022-03-07 14:54:07 +0800

    Update demo app features
    https://github.com/linahbei/chaos-monkey-monolithic/issues/1
    
    * Specification: https://hackmd.io/0ZJbhINSTmWp24IZQ5RY8g?view#%E9%96%8B%E5%A7%8B%E5%AF%A6%E4%BD%9C

--- kotlin-chaos-monkey/src/main/kotlin/Main.kt
+++ kotlin-chaos-monkey/src/main/kotlin/Main.kt
@@ -1,7 +1,29 @@
 import mono.MonoDaemon
+import java.util.*
 import kotlin.system.exitProcess
 
+fun daemonAppDemo(): Int {
+    val commands: Queue<String> = LinkedList()
+    val daemon = MonoDaemon(commands)
+    daemon.run()
+
+    //Add commands in rate limit overed time
+    commands.add("1")
+    commands.add("2")
+    commands.add("3")
+    commands.add("4")
+    commands.add("5")
+
+    //Add next command which followed rate limit constraint
+    Thread.sleep(MonoDaemon.RATE_LIMIT.toLong() * 1000)
+    commands.add("6")
+
+    //Manual stop daemon after hard code delay time
+    Thread.sleep(MonoDaemon.RATE_LIMIT.toLong() * 4 * 1000)
+    daemon.stop()
+    return MonoDaemon.EXIT_OK
+}
+
 fun main() {
-    val daemon = MonoDaemon()
-    exitProcess(daemon.run())
+    exitProcess(daemonAppDemo())
 }
--- kotlin-chaos-monkey/src/main/kotlin/mono/MonoDaemon.kt
+++ kotlin-chaos-monkey/src/main/kotlin/mono/MonoDaemon.kt
@@ -1,12 +1,67 @@
 package mono
 
-public class MonoDaemon {
+import java.lang.System.*
+import java.util.*
+import javax.naming.TimeLimitExceededException
+
+class MonoDaemon(commands: Queue<String>) {
     companion object {
         const val EXIT_OK = 0
+        const val RATE_LIMIT = 3
+    }
+    private var runner: Thread? = null
+    private var commands: Queue<String>? = null
+    private var latestExecutedTimestamp: Long
+
+    init {
+        this.commands = commands
+        latestExecutedTimestamp = currentTimeMillis()
     }
 
-    public fun run(): Int {
-        println("Hello, And Goodbye!")
-        return MonoDaemon.EXIT_OK
+    private fun assertRateLimit() {
+        val now: Long = currentTimeMillis()
+        if(RATE_LIMIT > now - latestExecutedTimestamp) {
+            throw TimeLimitExceededException()
+        }
+        latestExecutedTimestamp = now
+    }
+
+    private fun producer(command: String) {
+        try {
+            assertRateLimit()
+            println("Execute: %s".format(command))
+        } catch (timeLimitExceededException: TimeLimitExceededException) {
+            println("Drop: %s".format(command))
+        }
+    }
+
+    fun stop() {
+        if(null != runner && runner!!.isAlive) {
+            runner!!.interrupt()
+        }
+    }
+
+    fun forever() {
+        if(null != runner && runner!!.isAlive) {
+            runner!!.join()
+        }
+    }
+
+    fun run() {
+        if(null == runner || !runner!!.isAlive) {
+            runner = Thread {
+                while(true) {
+                    Thread.sleep(1)
+                    try {
+                        if( commands!!.isEmpty())
+                            continue
+                        producer(commands!!.poll())
+                    } catch (exception: Exception) {
+                        print(exception.message)
+                    }
+                }
+            }
+            runner!!.start()
+        }
     }
 }
